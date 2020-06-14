ddb94bf37dd844953c4983a839be7c988bd91bd7
SaadDamine <m.sdaminesaad@gmail.com>
2020-06-14 23:42:19 +0100

    calling  RESET API'sprogramatically
    (Rating from Rating service to catalog Service )

--- movie-catalog-service/src/main/java/com/damine/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/damine/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,10 +1,12 @@
 package com.damine.moviecatalogservice.resources;
 
 import com.damine.moviecatalogservice.models.CatalogItem;
+import com.damine.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,6 +15,16 @@ import java.util.List;
 public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        //get all rated movies
+        List<Rating> ratings= Arrays.asList(
+                new Rating("1",4),
+                new Rating("2",3)
+        );
+
+        // For each movie , call movie info services and get details
+
+        //put theme together
+
         return Collections.singletonList(
                 new CatalogItem("Transformers","trans des",4)
         );
