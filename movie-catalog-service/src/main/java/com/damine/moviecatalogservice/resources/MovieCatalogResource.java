e5ddd7059cd5fc5dc9916d33288dab340bca989e
SaadDamine <m.sdaminesaad@gmail.com>
2020-06-14 22:57:46 +0100

    create model catalog

--- movie-catalog-service/src/main/java/com/damine/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/damine/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,4 +1,20 @@
 package com.damine.moviecatalogservice.resources;
 
+import com.damine.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
+    @RequestMapping("/{userId}}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        return Collections.singletonList(
+                new CatalogItem("Transformers","trans des",4)
+        );
+    }
 }
