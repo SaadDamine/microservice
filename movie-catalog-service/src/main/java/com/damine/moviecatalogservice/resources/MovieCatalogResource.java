66d5e2b158ba30826ddd0168db51ecd36fe068c0
SaadDamine <m.sdaminesaad@gmail.com>
2020-06-15 00:04:21 +0100

    using REST Client Librery

--- movie-catalog-service/src/main/java/com/damine/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/damine/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,20 +1,26 @@
 package com.damine.moviecatalogservice.resources;
 
 import com.damine.moviecatalogservice.models.CatalogItem;
+import com.damine.moviecatalogservice.models.Movie;
 import com.damine.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        RestTemplate restTemplate=new RestTemplate();
+
+
         //get all rated movies
         List<Rating> ratings= Arrays.asList(
                 new Rating("1",4),
@@ -23,10 +29,15 @@ public class MovieCatalogResource {
 
         // For each movie , call movie info services and get details
 
+        return ratings.stream().map(rating ->{
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating, Movie.class);
+            return new CatalogItem(movie.getName(),"trans des",rating.getRating());})
+                .collect(Collectors.toList());
+
         //put theme together
 
-        return Collections.singletonList(
+       /* return Collections.singletonList(
                 new CatalogItem("Transformers","trans des",4)
-        );
+        );*/
     }
 }
