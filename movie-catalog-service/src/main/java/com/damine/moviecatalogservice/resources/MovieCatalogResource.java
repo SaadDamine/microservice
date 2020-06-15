4943da65bc1962aa95ccb9bafc43c22baf53f8ed
SaadDamine <m.sdaminesaad@gmail.com>
2020-06-15 06:56:10 +0100

    add LoadBalanced to the client service

--- movie-catalog-service/src/main/java/com/damine/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/damine/moviecatalogservice/resources/MovieCatalogResource.java
@@ -4,6 +4,7 @@ import com.damine.moviecatalogservice.models.CatalogItem;
 import com.damine.moviecatalogservice.models.Movie;
 import com.damine.moviecatalogservice.models.UserRatings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,19 +19,22 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private DiscoveryClient discoveryClient;
+
     @Autowired
-    private WebClient.Builder webClientBuilder;
+    private RestTemplate restTemplate;
+    /*@Autowired
+    private WebClient.Builder webClientBuilder;*/
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
         //get all rated movies
-        UserRatings ratings=restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId,UserRatings.class);
+        UserRatings ratings=restTemplate.getForObject("http://rating-data-service/ratingsdata/users/"+userId,UserRatings.class);
 
         // For each movie , call movie info services and get details
         return ratings.getUserRatings().stream().map(rating ->{
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating, Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating, Movie.class);
             //put theme together
             return new CatalogItem(movie.getName(),"trans des",rating.getRating());})
                 .collect(Collectors.toList());
